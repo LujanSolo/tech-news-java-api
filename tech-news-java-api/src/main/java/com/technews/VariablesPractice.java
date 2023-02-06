095df55c011733f4d6bcdfe026cf39d746b3dc1a
LujanSolo <lujansolo@proton.me>
2023-02-05 17:19:15 -0800

    added practice class for basic testing

--- tech-news-java-api/src/main/java/com/technews/VariablesPractice.java
+++ tech-news-java-api/src/main/java/com/technews/VariablesPractice.java
@@ -1,4 +1,15 @@
 package com.technews;
 
 public class VariablesPractice {
+    public static void main(String[] args){
+        int a = 3;
+        double b = 4.3;
+        double sum = a + b;
+        String phrase = "Price: ";
+        String concat = phrase + a;
+
+        System.out.println(sum);
+        System.out.println(phrase);
+        System.out.println(concat);
+    }
 }
