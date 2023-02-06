280fba0ca667dd20c9bdaf469d907f364c82b8de
LujanSolo <lujansolo@proton.me>
2023-02-05 17:04:37 -0800

    added two more classes for practice purposes

---
+++ tech-news-java-api/src/main/java/com/technews/VariableTypes.java
@@ -0,0 +1,11 @@
+package com.technews;
+
+public class VariableTypes {
+    public static void main(String[] args) {
+        String name = "Cleopatra";
+        int age = 2016;
+        double price = 1991.1123;
+        boolean truth = true;
+        boolean falseness = !truth;
+    }
+}
